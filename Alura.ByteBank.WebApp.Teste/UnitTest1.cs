360f2e70fe141bc5caa386aaee85e5bca2169245
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-20 10:07:56 -0300

    Criado projeto para testes com o webapp

---
+++ Alura.ByteBank.WebApp.Teste/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Teste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
